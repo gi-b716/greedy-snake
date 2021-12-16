6093f76e4883dfead43b58a0e15ec4843eaeefd1
gi-b716 <78394473+gi-b716@users.noreply.github.com>
2021-12-16 21:03:40 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,23 @@
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
+function 重置 () {
+    let list: number[] = []
+    状态 = 0
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    目标点 = [randint(0, 4), randint(0, 4)]
+    led.plot(list[0], list[1])
+}
+let 目标点: number[] = []
+let 状态 = 0
+重置()
 basic.forever(function () {
-    led.plot(randint(0, 4), randint(0, 4))
+    if (状态 == 0) {
+    	
+    } else {
+        重置()
+    }
 })
